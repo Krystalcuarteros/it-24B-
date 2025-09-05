0d45a7cca5131b882d218214d84adfd53034c5a6
Krystalcuarteros <charita121279@gmail.com>
2025-09-05 10:05:48 +0800

    Create Hello.js
    Added

---
+++ Hello.js
@@ -0,0 +1,11 @@
+console.log("Hi, my name is krystal cuarteros");
+
+num1 = 5
+num2 = 10
+num3 = 20
+
+sum = num1 + num2 + num3;
+
+console.log("The sum of " + num1 + " and " + num2 + " and " + num3 + " is = " + sum);
+
+return 0;
